364e9bb4b43d669044334813d6caa2f85f2a28fe
Ito Hiroaki <h.ito@iseed.jp>
2021-12-24 12:04:51 +0900

    [update]ログイン失敗時にエラーメッセージを表示

--- pages/log-in.vue
+++ pages/log-in.vue
@@ -11,8 +11,10 @@
           <label for="password">Password:</label>
           <input v-model="user.password" type="password" />
         </div>
+
         <button type="submit">ログイン</button>
       </form>
+      <div id="errorMessage"></div>
     </div>
   </div>
 </template>
@@ -27,12 +29,27 @@ export default {
       },
     }
   },
+  // mounted() {
+  //   alert('ページが読み込まれました！')
+  // },
   methods: {
-    loginUser() {
-      this.$auth.loginWith('local', {
-        data: this.user,
-      })
+    async loginUser() {
+      try {
+        await this.$auth.loginWith('local', {
+          data: this.user,
+        })
+        console.log('成功')
+      } catch {
+        document.getElementById('errorMessage').innerHTML =
+          'ログイン情報が間違っています'
+      }
     },
   },
 }
 </script>
+
+<style lang="css" scoped>
+#errorMessage {
+  color: red;
+}
+</style>

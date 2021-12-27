8a7630f96d281fce16b3daeea52e1c4b25dde735
Ito Hiroaki <h.ito@iseed.jp>
2021-12-27 17:56:12 +0900

    [add]プロジェクト一覧ページの作成

---
+++ pages/projects/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nuxt-link to="/projects/register">プロジェクトの新規追加</nuxt-link>
+    <MoleculesTableProjectList></MoleculesTableProjectList>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>

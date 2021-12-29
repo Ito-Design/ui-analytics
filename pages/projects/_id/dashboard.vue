5c5ba7eb0c683b036d0b3bf03ea57773be545315
Ito-Design <nekopon839@gmail.com>
2021-12-29 16:40:54 +0900

    [add]各プロジェクトのダッシュボードページを追加

---
+++ pages/projects/_id/dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>ダッシュボード</h1>
+    <p>{{ item.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData(context) {
+    const item = await context.$axios.$get('/api/project/' + context.params.id)
+    return { item }
+  },
+}
+</script>

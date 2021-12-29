58abf9e6940a9e30a45605580a30c3340702e0d7
Ito-Design <nekopon839@gmail.com>
2021-12-29 16:39:23 +0900

    [add]プロジェクト削除ボタンのコンポーネント作成

---
+++ components/atoms/ButtonProjectDel.vue
@@ -0,0 +1,27 @@
+<template>
+  <button @click="deleteRow(id)">プロジェクト{{ id }}の削除</button>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    deleteRow(id) {
+      this.$axios
+        .get('/api/project/del/' + id)
+        .then((response) => {
+          console.log(response)
+          this.$router.push('/projects')
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+  },
+}
+</script>

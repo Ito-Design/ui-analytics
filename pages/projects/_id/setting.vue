d83ef03b459b45ed3407619154a5f3633c60c936
Ito-Design <nekopon839@gmail.com>
2021-12-29 16:41:07 +0900

    [add]各プロジェクトの設定ページを追加

---
+++ pages/projects/_id/setting.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>プロジェクト設定</h1>
+    <p>プロジェクト名：{{ project.name }}</p>
+    <p>プロジェクトID：{{ project.id }}</p>
+    <AtomsButtonProjectDel :id="project.id"></AtomsButtonProjectDel>
+    <form @submit.prevent="updateProject">
+      <div class="form-group">
+        <label for="name">プロジェクト名</label>
+        <input v-model="updateData.name" />
+      </div>
+      <button type="submit">変更</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData(context) {
+    const project = await context.$axios.$get(
+      '/api/project/' + context.params.id
+    )
+    return { project }
+  },
+  data() {
+    return {
+      updateData: {
+        name: '',
+      },
+    }
+  },
+  methods: {
+    updateProject() {
+      this.$axios.$get(
+        '/api/project/update/' + this.$route.params.id,
+        this.updateData.name
+      )
+    },
+  },
+}
+</script>

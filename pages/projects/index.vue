db7a0d84bd0e123080e5318203256c141cd398db
Ito-Design <nekopon839@gmail.com>
2021-12-29 16:40:35 +0900

    [update]プロジェクト一覧ページにログアウトボタンの追加

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -2,6 +2,10 @@
   <div>
     <nuxt-link to="/projects/register">プロジェクトの新規追加</nuxt-link>
     <MoleculesTableProjectList></MoleculesTableProjectList>
+    <div v-if="$auth.loggedIn">
+      <p>{{ $store.state.auth.user }}</p>
+      <button @click="$auth.logout()">Logout</button>
+    </div>
   </div>
 </template>
 

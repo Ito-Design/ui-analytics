dac65a473ce2697d98e80570f5efadb6374ce05f
Ito-Design <nekopon839@gmail.com>
2022-01-12 18:22:48 +0900

    [fix]ボタン用アンカータグのコンポーネント化

---
+++ components/atoms/ButtonPrimaryAnchor.vue
@@ -0,0 +1,9 @@
+<template>
+  <nuxtLink
+    class="font-bold py-1 px-4 rounded bg-colorMain400 text-white hover:bg-colorMain300 inline-block"
+    v-bind="$attrs"
+    ><span class="mr-1">
+      <slot name="icon"></slot> </span
+    ><slot></slot
+  ></nuxtLink>
+</template>

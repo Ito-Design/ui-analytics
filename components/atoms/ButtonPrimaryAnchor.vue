078eadc7a82a05e6cc8a66ef5045759838213b00
Ito-Design <nekopon839@gmail.com>
2022-01-27 13:31:36 +0900

    [update]プライマリボタンのデザイン変更

--- components/atoms/ButtonPrimaryAnchor.vue
+++ components/atoms/ButtonPrimaryAnchor.vue
@@ -1,9 +1,7 @@
 <template>
   <nuxtLink
-    class="font-bold py-1 px-4 rounded bg-colorMain400 text-white hover:bg-colorMain300 inline-block"
+    class="font-bold py-1 px-4 rounded border border-transparent bg-colorMain400 text-white hover:bg-colorMain300 inline-block"
     v-bind="$attrs"
-    ><span class="mr-1">
-      <slot name="icon"></slot> </span
-    ><slot></slot
+    ><span class="mr-1"> <slot name="icon"></slot> </span><slot></slot
   ></nuxtLink>
 </template>

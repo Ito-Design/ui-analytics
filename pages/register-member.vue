31677ef999a1ee8e511e953cebc7092b57775121
Ito-Design <nekopon839@gmail.com>
2022-01-27 13:32:18 +0900

    [update]登録ページの構造変更

--- pages/register-member.vue
+++ pages/register-member.vue
@@ -1,23 +1,21 @@
 <template>
   <div>
-    <div>
-      <h1>ユーザ登録</h1>
-      <form @submit.prevent="registerUser">
-        <div class="form-group">
-          <label for="name">Name:</label>
-          <input v-model="user.name" />
-        </div>
-        <div class="form-group">
-          <label for="email">Email:</label>
-          <input v-model="user.email" />
-        </div>
-        <div class="form-group">
-          <label for="password">Password:</label>
-          <input v-model="user.password" type="password" />
-        </div>
-        <button type="submit">登録</button>
-      </form>
-    </div>
+    <h1>ユーザ登録</h1>
+    <form @submit.prevent="registerUser">
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <input v-model="user.name" />
+      </div>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input v-model="user.email" />
+      </div>
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input v-model="user.password" type="password" />
+      </div>
+      <button type="submit">登録</button>
+    </form>
   </div>
 </template>
 

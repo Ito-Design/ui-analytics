50cc99aff0b234df8270d31c48aaaeebf10d40a0
Ito-Design <nekopon839@gmail.com>
2022-01-27 10:47:34 +0900

    [improve]コメントアウト追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     extend: {
       colors: {
         colorMain500: '#00998E',
-        colorMain400: '#00A59A',
+        colorMain400: '#00A59A', // default
         colorMain300: '#28B3AA',
         colorMain200: '#90D3CF',
         colorMain100: '#E6F4F4',
@@ -11,7 +11,7 @@ module.exports = {
         colorBase400: '#AEAEAE',
         colorBase300: '#E9E9E9',
         colorBase200: '#F2F2F2',
-        colorBase100: '#FFFFFF',
+        colorBase100: '#FFFFFF', // white
       },
     },
   },

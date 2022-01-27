48d408c64be5cd10e3b7f5ad356611caa0db3745
Ito-Design <nekopon839@gmail.com>
2022-01-27 13:31:46 +0900

    [update]トップページ変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,39 @@
+<!-- Please remove this file from your project -->
 <template>
-  <Tutorial />
+  <div
+    class="relative flex items-top justify-center min-h-screen sm:items-center sm:pt-0"
+  >
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <AtomsLogo></AtomsLogo>
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <nav>
+          <nuxt-link to="/log-in">ログイン画面へ</nuxt-link>
+          <br />
+          <nuxt-link to="/register-member">ユーザー登録</nuxt-link>
+        </nav>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
   layout: 'notLogin',
+}
+</script>
+
+<script>
+export default {
+  layout: 'notLogin',
+  data() {
+    return {
+      user: {
+        name: 'Steve Harry',
+        email: 'steve@test.com',
+        password: 'password',
+      },
+    }
+  },
 }
 </script>

7d07d6164e6d088fefc996b4e22f3e2305c0b703
Ito-Design <nekopon839@gmail.com>
2022-01-27 13:28:51 +0900

    [add]input用のコンポーネント作成

---
+++ components/atoms/Input.vue
@@ -0,0 +1,17 @@
+<template>
+  <input
+    :value="value"
+    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    @input="$emit('input', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: '',
+    }
+  },
+}
+</script>
